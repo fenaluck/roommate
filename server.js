21aaa5ec3f5fe884c97e1d11206db3cf619e9937
fenaluck <fenaluck@gmail.com>
2022-02-26 12:13:02 -0300

    trabajo a medias

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // comensando con el trabajo
-const express = require ('express')
 const axios = require ('axios')
 const uuid = require ('uuid');
 const fs = require ('fs').promises;
+const express = require ('express')
 
 // despues creo mi app
 const app = express();
@@ -13,8 +13,6 @@ app.use(express.static('static'));
 // Se configura manejo de formularios
 app.use( express.json() );
 app.use( express.urlencoded({ extended: true }) );
-
-
 
 //obteniendo los datos
 async function newroommate(){
@@ -30,7 +28,7 @@ async function newroommate(){
     return newuser
 }
 
-// guardando la informacion d
+// guardando la informacion roomates
 app.post('/roommate', async(req, res)=>{
     
     let roommate = await newroommate()
@@ -43,45 +41,142 @@ app.post('/roommate', async(req, res)=>{
 
     //enviamos la informacion a la base de datos
     db.roommates.push(roommate)
-
     
-
     // finalmente guardo el nuevo usuario
     await fs.writeFile('db.json', JSON.stringify(db), 'utf-8') 
     res.send({todo:'ok'})
 
 });
 
-// post/ gastos
+// post gastos
 app.post('/gasto', async (req, res) => {
     let body;
     req.on('data', (payload) => {
       body = JSON.parse(payload);
+     
     });
-    req.on('end', () => {
-    // acá tenemos que crear el gasto
+    req.on('end', async() => {
+        
+        // creamos el gasto
+        const newgasto = {
+            id : uuid.v4().slice(30),
+            roommate : body.roommate,
+            descripcion : body.descripcion,
+            monto : body.monto
+        }
+        
+        //leemos el archivo creado
+        let db = await fs.readFile('db.json', 'utf-8')
+
+        //transformando de un string a un objeto
+        db = JSON.parse(db)
+
+        //enviamos la informacion a la base de datos
+        db.gastos.push(newgasto)
+
+
+        // finalmente guardo el nuevo gastos
+        await fs.writeFile('db.json', JSON.stringify(db), 'utf-8') 
+        res.send({todo:'ok'})
+    });
     
-    console.log(`aqui veremos ${db}`);
-    res.send({todo: 'OK'});
-    });
 });
 
-
-// get de roomate , get / gastos
+// get de roomate 
 app.get('/roommates', async(req, res)=>{
     let database = await fs.readFile('db.json', 'utf-8')
     database = JSON.parse(database)
     let roommates = database.roommates
     res.send({roommates})
 })
+// get de gastos
 app.get('/gastos', async(req, res)=>{
     let database = await fs.readFile('db.json', 'utf-8')
     database = JSON.parse(database)
     let gastos = database.gastos
-    console.log(gastos);
     res.send({gastos})
 })
 
+// modificar gastos
+app.put('/gasto', async (req, res) => {
+    //guardamos el id a modificar
+    const id = req.query.id;
+    
+    let body;
+    req.on('data', (payload) => {
+      body = JSON.parse(payload);
+     
+    });
+    req.on('end', async() => {
+                 
+        //leemos el archivo creado
+        let db = await fs.readFile('db.json', 'utf-8')
+
+        //transformando de un string a un objeto
+        db = JSON.parse(db)
+        
+        
+        let gasto = db.gastos.find(g=>g.id === id )
+        gasto.roommate = body.roommate
+        gasto.monto = body.monto
+        gasto.descripcion = body.descripcion
+        const roommate = db.roommates.find(r => r.nombre == body.roommate);
+        const gastosRoommate = db.gastos.filter( g => g.roommate = roommate.nombre).map(g => g.monto).reduce( (x, y) => x + y);
+        roommate.debe = gastosRoommate;
+        // finalmente guardo el nuevo gastos
+        await fs.writeFile('db.json', JSON.stringify(db), 'utf-8') 
+        res.send({todo:'ok'})
+    });
+    
+});
+/*
+// borrando archivo   
+app.delete('/eliminar', async (req, res)=>{
+    const id = req.query.id;   
+    console.log(id)
+    let body;
+    req.on('data', (payload) => {
+      body = JSON.parse(payload);
+     
+    });
+    req.on('end', async() => {
+              
+        //leemos el archivo creado
+        let db = await fs.readFile('db.json', 'utf-8')
+
+        //transformando de un string a un objeto
+        db = JSON.parse(db)
+
+        const gasto = db.gastos.find(g=>g.id === id )
+        console.log(gasto)
+        gasto.roommate = body.roommate
+        gasto.monto = body.monto
+        gasto.descripcion = body.descripcion
+        const roommate = db.roommates.find(r => r.nombre == body.roommate);
+        const gastosRoommate = db.gastos.filter( g => g.roommate = roommate.nombre).map(g => g.monto).reduce( (x, y) => x + y);
+        roommate.debe = gastosRoommate;
+        
+        
+        // borramos el archivo
+        await fs.unlike('db.json', JSON.stringify(db), 'utf-8') 
+        res.send({todo:'ok'})
+        
+    });   
+         
+})
+*/
+// borrando archivo   
+app.get('/eliminar', (req, res)=>{
+    const id = req.query.id; 
+    console.log(id);
+    const archivo = `db.json/${req.query.id}`;
+    fs.unlink(archivo, function(){
+        
+        res.send('archivo borrado');
+    });  
+    console.log('archivo borrado correctamente');
+     
+})
 
 //correr el servidor
 app.listen(3000, function(){
